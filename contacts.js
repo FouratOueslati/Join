da5feba4ec611e0fd3f005c0db9d0b2d159ae343
Nicole Gerlach <coleni82@gmail.com>
2024-06-02 17:20:32 +0200

    edit contacts + css

--- contacts.js
+++ contacts.js
@@ -37,10 +37,12 @@ function displayInitialsAndContacts() {
             let name = contacts[i]["name"];
             let surname = contacts[i]["surname"];
             let email = contacts[i]["email"];
+            let phonenumber = contacts[i]["number"];
             let firstLetterOfName = name.charAt(0);
             let firstLetterOfSurname = surname.charAt(0);
             if (contactInitial.innerHTML === firstLetterOfName) {
-                contactsContainer.innerHTML += getContactsContainerHtml(i, firstLetterOfName, firstLetterOfSurname, name, surname, email);
+                contactsContainer.innerHTML += getContactsContainerHtml(i, firstLetterOfName, firstLetterOfSurname, name, surname, email, phonenumber);
+            addColorToNewContact(i)
             }
         }
     }
@@ -49,7 +51,7 @@ function displayInitialsAndContacts() {
 
 function getContactsContainerHtml(i, firstLetterOfName, firstLetterOfSurname, name, surname, email) {
     return `
-    <div class="contact-data">
+    <div id="contactData${i}" class="contact-data" onclick="openContact(${i})">
         <div id="contactsInitials${i}" class="shorts-name">${firstLetterOfName}${firstLetterOfSurname}</div>
         <div>
             <div id="contact-name${i}" class="contact-name">${name} ${surname}</div>
@@ -57,5 +59,77 @@ function getContactsContainerHtml(i, firstLetterOfName, firstLetterOfSurname, na
         </div>
     </div>
     `;
+}
+
+function openContact(i) {
+    let contactData = document.getElementById(`contactData${i}`);
+    let firstLetterOfName = contactData.querySelector('.shorts-name').textContent.charAt(0);
+    let firstLetterOfSurname = contactData.querySelector('.shorts-name').textContent.charAt(1);
+    let name = contactData.querySelector('.contact-name').textContent;
+    let email = contactData.querySelector('.contact-email').textContent;
+    let phonenumber = contacts[i]["number"];
+    
+    let contactInfos = document.getElementById('contactInfos');
+    contactInfos.innerHTML = '';
+    contactInfos.innerHTML += getContactInfosHtml(firstLetterOfName, firstLetterOfSurname, name, email, phonenumber);
+    addColorToNewContact(i);
+}
+
+
+function getContactInfosHtml(firstLetterOfName, firstLetterOfSurname, name, surname, phonenumber, i) {
+    return `
+    <div>
+        <div class="edit-delete-contact">
+            <div id="contactsInitialsBig${i}" class="shorts-name-big">${firstLetterOfName}${firstLetterOfSurname}</div>
+            <div class="full-name">${name}
+                <div class="edit-delete-box">
+                    <img src="./img/edit_contacts.png">
+                    <img src="./img/delete_contact.png">
+                </div>
+            </div>
+        </div>
+        <div class="contact-information">Contact Information</div>
+        <div class="email-phone-box">
+            <div class="email-phone-headline">Email</div>
+            <div class="email-phone join">${surname}</div>
+            <div class="email-phone-headline">Phone</div>
+            <div class="email-phone">${phonenumber}</div>
+        </div>
+    </div>
+    `;
+}
+
+
+function getRandomPastelColor() {
+    var r = Math.floor(Math.random() * 256); // Rote Komponente
+    var g = Math.floor(Math.random() * 256); // Grüne Komponente
+    var b = Math.floor(Math.random() * 256); // Blaue Komponente
+    
+
+    // // Umwandlung in Pastellfarben durch Hinzufügen von Weiß
+    // r = Math.floor((r + 255) / 2);
+    // g = Math.floor((g + 255) / 2);
+    // b = Math.floor((b + 255) / 2);
+
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+
+function addColorToNewContact(i) {
+    let newShort = document.getElementById(`contactsInitials${i}`);
+    if (!newShort.style.backgroundColor) { // Überprüfen, ob noch keine Hintergrundfarbe zugewiesen wurde
+        newShort.style.backgroundColor = getRandomPastelColor();
+    }
+}
+
+
+function addNewContact() {
+    document.getElementById('dialogNewContact').classList.remove('d-none');
+}
+
+
+function closeDialog() {
+    document.getElementById('dialogNewContact').classList.add('d-none');
+    document.getElementById('contactInfos').classList.add('d-none');
 }
 

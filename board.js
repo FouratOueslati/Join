a37620d3b4c4deb6ed92ce1024e34a012844bd16
FouratOueslati <fourat.oueslati@gmx.de>
2024-09-04 20:37:02 +0200

    tide up

--- board.js
+++ board.js
@@ -134,15 +134,21 @@ async function getContactInitials(contacts, i) {
  * @param {number} i 
  */
 function generatePriorityImgOpened(i) {
-    let priority = document.getElementById(`openedPriority${i}`);
-    let img = document.getElementById(`priorityImg${i}`);
-    if (priority && priority.innerHTML === 'Medium') {
-        img.src = "./addTaskImg/mediu.svg";
-    } else if (priority && priority.innerHTML === 'Low') {
-        img.src = "./addTaskImg/low.svg";
-    } else if (priority && priority.innerHTML === 'Urgent') {
-        img.src = "./addTaskImg/high.svg";
+    const priority = document.getElementById(`openedPriority${i}`);
+    const img = document.getElementById(`priorityImg${i}`);
+    if (!img) return;
+    let imgSrc = "./addTaskImg/low.svg";
+    if (priority) {
+        switch (priority.innerHTML) {
+            case 'Medium':
+                imgSrc = "./addTaskImg/mediu.svg";
+                break;
+            case 'Urgent':
+                imgSrc = "./addTaskImg/high.svg";
+                break;
+        }
     }
+    img.src = imgSrc;
 }
 
 
@@ -349,9 +355,9 @@ async function editTask(i) {
     addEventListenerDropDown();
     addPrioEventListenersEdit();
     changeColor(document.querySelector('.button-prio-selected'));
-    //onInputChangeEdit();
     displayNamesOfContactsEdit();
     displayAssignedContactsInEdit();
+    onInputChangeEdit();
 }
 
 
@@ -394,262 +400,5 @@ async function displayAssignedContactsInEdit() {
             let initials = getInitials(name)
             containerBubbleInitials.innerHTML += generateBubbleInitialsHtml(i, initials, backgroundColor);
         }
-    }
-}
-
-
-/**
- * This function changes the color of the selected priority button
- * 
- * @param {element} clickedButton 
- */
-function changeColorEdit(clickedButton) {
-    const buttons = [
-        { element: document.getElementById('lowButtonEdit'), class: 'lowSelected' },
-        { element: document.getElementById('mediumButtonEdit'), class: 'mediumSelected' },
-        { element: document.getElementById('urgentButtonEdit'), class: 'urgentSelected' }
-    ];
-    buttons.forEach(button => {
-        if (button.element) {
-            button.element.classList.toggle(button.class, button.element === clickedButton);
-            if (button.element !== clickedButton) {
-                button.element.classList.remove(button.class);
-            }
-        }
-    });
-}
-
-
-function addSubtaskEdit(i) {
-    let container = document.getElementById(`subtasksContainer${i}`);
-    let subtaskText = document.getElementById('inputFieldSubtaskEdit').value.trim();
-    if (subtaskText !== '') {
-        let newSubtask = { text: subtaskText, status: 'pending' };
-        if (todos[i]) {
-            if (!todos[i].task) {
-                todos[i].task = { subtasks: [] };
-            } if (!Array.isArray(todos[i].task.subtasks)) {
-                todos[i].task.subtasks = [];
-            }
-            if (editedSubtask !== null) {
-                todos[editedSubtask.taskIndex].task.subtasks.splice(editedSubtask.subtaskIndex, 0, newSubtask);
-                editedSubtask = null;
-            } else {
-                todos[i].task.subtasks.push(newSubtask);
-            }
-            localStorage.setItem('todos', JSON.stringify(todos));
-            container.innerHTML = generateSubtasksEditHtml(todos[i].task.subtasks, i);
-            document.getElementById('inputFieldSubtaskEdit').value = '';
-        }
-    }
-    onInputChangeEdit();
-}
-
-
-/**
- * This function edit the subtasks and saves them in local storage
- * 
- * @param {number} taskIndex 
- * @param {number} subtaskIndex 
- */
-function editSubtaskEdit(taskIndex, subtaskIndex) {
-    let subtaskDiv = document.getElementById(`subtask${taskIndex}-${subtaskIndex}`);
-    let text = subtaskDiv.innerHTML;
-    document.getElementById('inputFieldSubtaskEdit').value = text;
-    editedSubtask = { taskIndex, subtaskIndex, text };
-    todos[taskIndex].task.subtasks.splice(subtaskIndex, 1);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    displaySubtasksEdit(taskIndex);
-    onInputChangeEdit();
-}
-
-
-/**
- * This function dispays the edited subtask
- * 
- * @param {number} i 
- */
-function displaySubtasksEdit(i) {
-    const container = document.getElementById(`subtasksContainer${i}`);
-    container.innerHTML = generateSubtasksEditHtml(todos[i].task.subtasks, i);
-}
-
-
-/**
- * This function deletes the input field for the subtask
- */
-function clearSubtaskInputEdit() {
-    let inputField = document.getElementById('inputFieldSubtaskEdit');
-    inputField.value = '';
-    if (editedSubtask !== null) {
-        todos[editedSubtask.taskIndex].task.subtasks.splice(editedSubtask.subtaskIndex, 0, { text: editedSubtask.text, status: 'pending' });
-        localStorage.setItem('todos', JSON.stringify(todos));
-        displaySubtasksEdit(editedSubtask.taskIndex);
-        editedSubtask = null;
-    }
-    onInputChangeEdit();
-}
-
-
-/**
- * Thias function delete the subtask
- * 
- * @param {number} taskIndex 
- * @param {number} subtaskIndex 
- */
-function deleteSubtaskEdit(taskIndex, subtaskIndex) {
-    let subtasks = todos[taskIndex].task.subtasks;
-    subtasks.splice(subtaskIndex, 1);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    displaySubtasksEdit(taskIndex);
-}
-
-
-/**
- * This function get the assigned contacts to save them
- * 
- * @returns {object}
- */
-function getTaskContacts() {
-    let newContacts;
-    const newlyAssignedContacts = JSON.parse(localStorage.getItem('contacts')) || '[]';
-    const toBeEditedAssignedContacts = JSON.parse(localStorage.getItem('toBeEditedAssignedContacts')) || '[]';
-    if (newlyAssignedContacts.length > 0) {
-        newContacts = newlyAssignedContacts;
-    } else {
-        newContacts = toBeEditedAssignedContacts;
-    }
-    return newContacts;
-}
-
-
-/**
- * This function gets the assigned priority for saving
- * 
- * @returns {element}
- */
-function getTaskPriority() {
-    const priority = localStorage.getItem('lastClickedButton');
-    const toBeEditedPriority = localStorage.getItem('toBeEditedPriority');
-    let newPriority;
-    if (toBeEditedPriority === priority) {
-        newPriority = toBeEditedPriority;
-    } else {
-        newPriority = priority;
-    }
-    return newPriority;
-}
-
-
-/**
- * This function gets the assigned subtask for saving
- * 
- * @param {number} i 
- * @returns {string}
- */
-function getTaskSubtasks(i) {
-    const subtasksContainer = document.getElementById(`subtasksContainer${i}`);
-    const subtaskDivs = subtasksContainer.getElementsByClassName('subtask-Txt');
-    const subtasks = [];
-    for (let j = 0; j < subtaskDivs.length; j++) {
-        const subtaskText = subtaskDivs[j].querySelector(`#subtask${i}-${j}`).innerText;
-        subtasks.push({ text: subtaskText, status: 'undone' });
-    }
-    return subtasks;
-}
-
-
-/**
- * This function gets the details of the task (description, name and date) for saving
- * 
- * @param {number} i 
- * @returns {string, number}
- */
-function getTaskDetails(i) {
-    const nameEdit = document.getElementById(`taskTitleEdit${i}`).value;
-    const descriptionEdit = document.getElementById(`taskDescriptionEdit${i}`).value;
-    const dateEdit = document.getElementById(`dateEdit${i}`).value;
-    let details = {};
-    if (nameEdit && descriptionEdit && dateEdit) {
-        details = { nameEdit, descriptionEdit, dateEdit };
-    }
-    return details;
-}
-
-
-function showCheckedContacts() {
-    let assignedContactsJson = localStorage.getItem('toBeEditedAssignedContacts');
-    if (!assignedContactsJson) {
-        return;
-    }
-    let assignedContacts = JSON.parse(assignedContactsJson);
-    let assignedContactNames = assignedContacts.map(contact => contact.name);
-    let contacts = document.querySelectorAll('[id^="contact-"]');
-    let checkboxes = document.querySelectorAll('[id^="checkbox"]');
-    let contactCheckboxMap = {};
-    contacts.forEach(contact => {
-        let contactName = contact.innerHTML.trim();
-        let contactIndex = contact.id.split('-')[1];
-        contactCheckboxMap[contactIndex] = contactName;
-    });
-    checkboxes.forEach(checkbox => {
-        let checkboxIndex = checkbox.id.replace('checkbox', '');
-        let contactName = contactCheckboxMap[checkboxIndex];
-
-        if (contactName) {
-            checkbox.checked = assignedContactNames.includes(contactName);
-        } else {
-            checkbox.checked = false;
-        }
-    });
-}
-
-
-async function saveTask(i) {
-    const toBeEditedTaskId = localStorage.getItem('toBeEditedTaskId');
-    const toBeEditedDragCategory = JSON.parse(localStorage.getItem('toBeEditedDragCategory'));
-    const toBeEditedCategory = JSON.parse(localStorage.getItem('toBeEditedCategory'));
-    const newContacts = getTaskContacts();
-    const newPriority = getTaskPriority();
-    const { nameEdit, descriptionEdit, dateEdit } = getTaskDetails(i);
-    const subtasks = getTaskSubtasks(i);
-    const task = {
-        name: nameEdit,
-        description: descriptionEdit,
-        date: dateEdit,
-        contacts: newContacts,
-        category: toBeEditedCategory,
-        dragCategory: toBeEditedDragCategory,
-        subtasks: subtasks,
-        priority: newPriority
-    };
-    await updateUserTasks(uid, toBeEditedTaskId, task);
-    await displayOpenTasks();
-    localStorage.removeItem('contacts');
-}
-
-function validateAndAddTask() {
-    const taskTitle = document.getElementById('taskTitle');
-    const date = document.getElementById('date');
-    const categoryContainer = document.getElementById('selectCategoryContainer');
-    const category = document.getElementById('selectCategory');
-    taskTitle.style.borderColor = '';
-    date.style.borderColor = '';
-    categoryContainer.style.borderColor = '';
-    let isValid = true;
-    if (taskTitle.value.trim().length < 4) {
-        taskTitle.style.borderColor = 'red';
-        isValid = false;
-    }
-    if (!date.value) {
-        date.style.borderColor = 'red';
-        isValid = false;
-    }
-    if (category.textContent === 'Select task category') {
-        categoryContainer.style.borderColor = 'red';
-        isValid = false;
-    }
-    if (isValid) {
-        addTask();
     }
 }
---
+++ board3.js
@@ -0,0 +1,264 @@
+/**
+ * This function sets the background color for the selected category
+ * 
+ * @param {number} i 
+ */
+function setCategoryColor(i) {
+    let categoryContainer = document.getElementById(`category${i}`);
+    if (categoryContainer && categoryContainer.innerHTML === 'Technical Task') {
+        categoryContainer.style.backgroundColor = 'rgb(31, 215, 193)';
+    } else if (categoryContainer && categoryContainer.innerHTML === 'User Story') {
+        categoryContainer.style.backgroundColor = 'rgb(0, 56, 255)';
+    } else if (categoryContainer && categoryContainer.innerHTML === 'Design') {
+        categoryContainer.style.backgroundColor = 'rgb(255,211,155)';
+    }
+}
+
+
+/**
+ * This function displays the background color for the selected category in the larger view 
+ * of the task
+ * 
+ * @param {number} i 
+ */
+function setCategoryColorOpened(i) {
+    let categoryContainerOpened = document.getElementById(`categoryOpened${i}`);
+    if (categoryContainerOpened && categoryContainerOpened.innerHTML === 'Technical Task') {
+        categoryContainerOpened.style.backgroundColor = 'rgb(31, 215, 193)';
+    } else if (categoryContainerOpened && categoryContainerOpened.innerHTML === 'User Story') {
+        categoryContainerOpened.style.backgroundColor = 'rgb(0, 56, 255)';
+    } else if (categoryContainerOpened && categoryContainerOpened.innerHTML === 'Design') {
+        categoryContainerOpened.style.backgroundColor = 'rgb(255,211,155)';
+    }
+}
+
+
+/**
+ * This function toggle the status of the subtask
+ * 
+ * @param {number} i 
+ * @param {number} j 
+ */
+async function toggleSubtaskStatus(i, j) {
+    let subtaskCheckbox = document.getElementById(`subtaskCheckbox(${i}, ${j})`);
+    localStorage.setItem(`subtaskCheck(${i}, ${j})`, subtaskCheckbox.checked);
+    let statusOfSubtask = JSON.parse(localStorage.getItem(`subtaskCheck(${i}, ${j})`));
+    let userData = await loadSpecificUserDataFromLocalStorage();
+    let tasks = userData.tasks;
+    let taskIds = Object.keys(tasks);
+    let id = taskIds[i];
+    let task = tasks[id];
+    await updateSubtaskStatus(tasks, i, j, statusOfSubtask);
+    await generateNumberOfSubtasks(i, task);
+    updateLoadBar(i);
+}
+
+
+/**
+ * This function updates the status of subtask
+ * 
+ * @param {object} tasks 
+ * @param {number} i 
+ * @param {number} j 
+ * @param {*element} statusOfSubtask 
+ */
+async function updateSubtaskStatus(tasks, i, j, statusOfSubtask) {
+    let taskIds = Object.keys(tasks);
+    let taskId = taskIds[i];
+    let task = tasks[taskId];
+    let subtasks = task.subtasks;
+    let subtaskIds = Object.keys(subtasks);
+    let subtaskId = subtaskIds[j];
+    let subtask = subtasks[subtaskId];
+    subtask.status = statusOfSubtask ? 'done' : 'undone';
+    await updateSubtaskStatusInFirebase(subtask.status, taskId, subtaskId);
+}
+
+
+/**
+ * This function updates the subtask status in external storage
+ * 
+ * @param {element} status 
+ * @param {number} taskId 
+ * @param {number} subtaskId 
+ */
+async function updateSubtaskStatusInFirebase(status, taskId, subtaskId) {
+    let userData = await loadSpecificUserDataFromLocalStorage();
+    let tasks = userData.tasks;
+    if (tasks[taskId] && tasks[taskId].subtasks[subtaskId]) {
+        tasks[taskId].subtasks[subtaskId].status = status;
+        await updateUserData(uid, userData);
+    }
+}
+
+/**
+ * This function updates the loadbar
+ * 
+ * @param {number} i 
+ */
+function updateLoadBar(i) {
+    const loadBarContainer = document.getElementById(`loadBarContainer${i}`);
+    const loadBar = document.getElementById(`loadBar${i}`);
+    const subtaskNumber = document.getElementById(`subtasksNumber${i}`);
+    const subtaskText = subtaskNumber.innerHTML;
+    const match = subtaskText.match(/(\d+)\/(\d+) Subtasks/);
+    if (match) {
+        const completedSubtasks = parseInt(match[1], 10);
+        const totalSubtasks = parseInt(match[2], 10);
+        const percentage = (completedSubtasks / totalSubtasks) * 100;
+        loadBar.style.width = `${percentage}%`;
+    } else {
+        loadBarContainer.style.display = 'none';
+        loadBar.style.width = '0%';
+    }
+}
+
+
+
+/**
+ * This function filters searched tasks
+ */
+function filterTask() {
+    let clickHere = document.getElementById('clickHere');
+    let searchInput = document.getElementById('search');
+    let search = searchInput.value.toLowerCase();
+    if (search.length >= 3) {
+        clickHere.classList.remove('display-none-a');
+        filterWithSearchTerm(search.slice(0, 3));
+        removeSpecificColorFromDragArea();
+    } else if (search.length === 0) {
+        clearClickHere();
+        removeSpecificColorFromDragArea();
+    }
+}
+
+
+/**
+ * This function displays searched tasks based on words entered
+ * 
+ * @param {string} searchTerm 
+ */
+function filterWithSearchTerm(searchTerm) {
+    let matchingTaskCount = 0;
+    for (let i = 0; i < todos.length; i++) {
+        let taskTitleElement = document.getElementById(`taskTitle${i}`);
+        let taskCard = document.getElementById(`task${i}`);
+        if (taskTitleElement && taskCard) {
+            let taskTitle = taskTitleElement.innerHTML.toLowerCase().slice(0, 3);
+            if (taskTitle.includes(searchTerm)) {
+                taskCard.style.display = 'block';
+                matchingTaskCount++;
+            } else {
+                taskCard.style.display = 'none';
+            }
+        }
+    }
+    document.getElementById('taskCount').innerText = matchingTaskCount;
+}
+
+
+/**
+ * This function clears the serach input and displays all tasks
+ */
+function clearClickHere() {
+    let clickHere = document.getElementById('clickHere');
+    clickHere.classList.add('display-none-a');
+    for (let i = 0; i < todos.length; i++) {
+        let taskCard = document.getElementById(`task${i}`);
+        if (taskCard) {
+            taskCard.style.display = 'block';
+        }
+    }
+    document.getElementById('taskCount').innerText = '0';
+}
+
+
+/**
+ * This function manages the drag and drop state
+ * 
+ * @param {number} id 
+ */
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+
+/**
+ * This function allows tasks can be postponed
+ * 
+ * @param {element} category 
+ */
+async function moveTo(category) {
+    todos[currentDraggedElement]['task']['dragCategory'] = category;
+    await updateDragCategoryInFirebase(category, todos[currentDraggedElement].id);
+    removeTaskFromContainer(currentDraggedElement);
+    addTaskToContainer(currentDraggedElement, category);
+    await displayOpenTasks()
+}
+
+
+/**
+ * This function allows tasks to be remove from a container
+ * 
+ * @param {number} index 
+ */
+function removeTaskFromContainer(index) {
+    const taskElement = document.getElementById(`task${index}`);
+    if (taskElement) {
+        taskElement.remove();
+        const container = taskElement.parentElement;
+        if (container && container.children.length === 0) {
+            const noTaskMessage = container.querySelector('.drag-area-text');
+            if (noTaskMessage) noTaskMessage.style.display = 'block';
+        }
+    }
+}
+
+
+function addTaskToContainer(index, category) {
+    const containerIdMap = {
+        'todo': 'toDoTasks',
+        'inprogress': 'inProgressTasks',
+        'awaitfeedback': 'feedbackTasks',
+        'done': 'done'
+    };
+    const containerId = containerIdMap[category];
+    const container = document.getElementById(containerId);
+    if (container) {
+        container.classList.remove('drag-area-no-elements');
+        container.classList.add('drag-area-has-elements');
+        const noTaskMessage = container.querySelector('.drag-area-text');
+        if (noTaskMessage) noTaskMessage.style.display = 'none';
+        container.innerHTML += getToDoTaskHtml(todos[index], index);
+        getContactInitials(todos[index].task.contacts, index);
+        generateNumberOfSubtasks(index, todos[index]);
+        generatePriorityImgUnopened(index, todos[index]);
+        updateLoadBar(index);
+    }
+}
+
+/**
+ * This function checks the drag and drop container and displays the tasks or information
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    const containers = document.querySelectorAll('.drag-area');
+    containers.forEach(container => {
+        const tasks = container.querySelectorAll('.todo-class');
+        const noTaskMessage = container.querySelector('.drag-area-text');
+        if (tasks.length === 0 && noTaskMessage) {
+            noTaskMessage.style.display = 'block';
+        } else if (noTaskMessage) {
+            noTaskMessage.style.display = 'none';
+        }
+    });
+});
+
+
+/**
+ * This function makes it possible to drag and drop tasks into a container
+ * 
+ * @param {*} ev 
+ */
+function allowDrop(ev) {
+    ev.preventDefault();
+}
